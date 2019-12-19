397bf2f9bbf2145edb6453041b3598100eb3c910
Krzysztof Polek <krzysztof-polek@wp.pl>
2019-12-19 12:15:40 +0100

    Add task2 in chapter2

---
+++ src/main/java/chapter2/Task2.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+//      Przyjrzyj się metodzie nextInt z klasy Scanner. Czy jest to metoda dostępowa,
+//      czy modyfikująca? Dlaczego? A jak jest z metodą nextInt z klasy Random?
+
+        System.out.println("Metody nextInt w klasach Scanner oraz Random są");
+        System.out.println("metodami dostępowymi, ponieważ ich wywołanie nie ma");
+        System.out.println("wpłwu na zmianę wartości przekazywanego parametru.");
+
+    }
+
+}
+
